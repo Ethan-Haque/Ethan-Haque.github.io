425cdf10fbcae74bee7a1194ec44b2e9381be8a4
Ethan Haque <eth.haque@gmail.com>
2022-09-03 19:30:58 -0700

    Update assets, lower file sizes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
           Ethan Haque.
         </h1>
         <h2 className="text-4xl sm:text-7xl text-[#ccd6f6]">
-          I'm a Junior Developer.
+          I'm a Full Stack Developer.
         </h2>
         <p className="text-[#8892b0] py-4 max-w-[700px]">
           // Profile
